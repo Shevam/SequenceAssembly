b2b69f3ccc20f5d47050fd6dc59f75e0fb90cd0d
azhar759 <azhar_01@live.com>
2015-03-23 21:48:21 +0400

    update chart on assembler execution

--- src/assembler/Main.java
+++ src/assembler/Main.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Main 
 {
-	public enum AssemblyMethods { DE_BRUIJN, OVERLAP, GREEDY, IMPROVED_DE_BRUIJN };
+	public enum AssemblyMethods { DE_BRUIJN, OVERLAP, GREEDY,IMPROVED_DE_BRUIJN; };
 	
 	static final String SEQUENCE_FILE = "BorreliaFull_CompleteSequence.fasta";
 	static final String READS_FILE_NAME = "generatedReads.fasta";
@@ -12,31 +12,41 @@ public class Main
 	static final int KMER_SIZE = 41;
 	
 	static long programStartTime, programEndTime;
-	static long graphConstructionStartTime, graphConstructionEndTime;
-	static long contigGenerationStartTime, contigGenerationEndTime;
 	static AssemblyMethods assemblyMethods[];
+	
+	static long dbgGraphConstructionStartTime = 0, dbgGraphConstructionEndTime = 0;
+	static long overlapGraphConstructionStartTime = 0, overlapGraphConstructionEndTime = 0;
+	static long greedyGraphConstructionStartTime = 0, greedyGraphConstructionEndTime = 0;
+	static long impDbgGraphConstructionStartTime = 0, impDbgGraphConstructionEndTime = 0;
+	
+	static long dbgContigGenerationStartTime = 0, dbgContigGenerationEndTime = 0;
+	static long overlapContigGenerationStartTime = 0, overlapContigGenerationEndTime = 0;
+	static long greedyContigGenerationStartTime = 0, greedyContigGenerationEndTime = 0;
+	static long impDbgContigGenerationStartTime = 0, impDbgContigGenerationEndTime = 0;
+	
+	public static boolean isCompleted = false;
 	
 	public static void main(String args[]) 
 	{
-		assemblyMethods = new AssemblyMethods[] { AssemblyMethods.IMPROVED_DE_BRUIJN };//, AssemblyMethods.OVERLAP, AssemblyMethods.DE_BRUIJN, AssemblyMethods.IMPROVED_DE_BRUIJN };
+		assemblyMethods = new AssemblyMethods[] { AssemblyMethods.GREEDY, AssemblyMethods.OVERLAP };
 		
 		programStartTime = System.nanoTime();
-		
+		isCompleted = false;
 		for (AssemblyMethods method : assemblyMethods) {
 			switch (method) {
 			case DE_BRUIJN:
 				System.out.println("Method: DeBruijnGraph");
 				
-				graphConstructionStartTime = System.nanoTime();
+				dbgGraphConstructionStartTime = System.nanoTime();
 				TypicalDeBruijnGraph dbg = new TypicalDeBruijnGraph();
 				dbg.constructGraph(new File(READS_FILE_NAME), KMER_SIZE);
-				graphConstructionEndTime = System.nanoTime();
-				System.out.println("Time to construct graph(ms): " + (graphConstructionEndTime - graphConstructionStartTime) / 1000000);
+				dbgGraphConstructionEndTime = System.nanoTime();
+				System.out.println("Time to construct graph(ms): " + (dbgGraphConstructionEndTime - dbgGraphConstructionStartTime) / 1000000);
 				
-				contigGenerationStartTime = System.nanoTime();
+				dbgContigGenerationStartTime = System.nanoTime();
 				dbg.traverseGraphToGenerateContigs(GENERATED_CONTIGS_FILE);
-				contigGenerationEndTime = System.nanoTime();
-				System.out.println("Time to generate contigs(ms): " + (contigGenerationEndTime - contigGenerationStartTime) / 1000000);
+				dbgContigGenerationEndTime = System.nanoTime();
+				System.out.println("Time to generate contigs(ms): " + (dbgContigGenerationEndTime - dbgContigGenerationStartTime) / 1000000);
 				
 				dbg = null;
 				System.gc();
@@ -44,82 +54,48 @@ public class Main
 				
 			case OVERLAP:
 				System.out.println("Method: OverlapGraph");
-				graphConstructionStartTime = System.nanoTime();
+				overlapGraphConstructionStartTime = System.nanoTime();
 				OlcOverlapGraph oog = new OlcOverlapGraph(MINIMUM_OVERLAP_LENGTH);
 				oog.constructGraph(new File(READS_FILE_NAME), MINIMUM_OVERLAP_LENGTH);
-				graphConstructionEndTime = System.nanoTime();
-				System.out.println("Time to construct graph(ms): " + (graphConstructionEndTime - graphConstructionStartTime) / 1000000);
+				overlapGraphConstructionEndTime = System.nanoTime();
+				System.out.println("Time to construct graph(ms): " + (overlapGraphConstructionEndTime - overlapGraphConstructionStartTime) / 1000000);
 				
-				contigGenerationStartTime = System.nanoTime();
+				overlapContigGenerationStartTime = System.nanoTime();
 				oog.traverseGraphToGenerateContigs(GENERATED_CONTIGS_FILE);
-				contigGenerationEndTime = System.nanoTime();
-				System.out.println("Time to generate contigs(ms): " + (contigGenerationEndTime - contigGenerationStartTime) / 1000000);
+				overlapContigGenerationEndTime = System.nanoTime();
+				System.out.println("Time to generate contigs(ms): " + (overlapContigGenerationEndTime - overlapContigGenerationStartTime) / 1000000);
 				oog = null;
 				System.gc();
 				break;
 				
 			case GREEDY:
 				System.out.println("Method: Greedy");
-				graphConstructionStartTime = System.nanoTime();
+				greedyGraphConstructionStartTime = System.nanoTime();
 				GreedyOverlapGraph gog = new GreedyOverlapGraph(MINIMUM_OVERLAP_LENGTH);
 				gog.constructGraph(new File(READS_FILE_NAME), MINIMUM_OVERLAP_LENGTH);
-				graphConstructionEndTime = System.nanoTime();
-				System.out.println("Time to construct graph(ms): " + (graphConstructionEndTime - graphConstructionStartTime) / 1000000);
+				greedyGraphConstructionEndTime = System.nanoTime();
+				System.out.println("Time to construct graph(ms): " + (greedyGraphConstructionEndTime - greedyGraphConstructionStartTime) / 1000000);
 				
-				contigGenerationStartTime = System.nanoTime();
+				greedyContigGenerationStartTime = System.nanoTime();
 				gog.traverseGraphToGenerateContigs(GENERATED_CONTIGS_FILE);
-				contigGenerationEndTime = System.nanoTime();
-				System.out.println("Time to generate contigs(ms): " + (contigGenerationEndTime - contigGenerationStartTime) / 1000000);
+				greedyContigGenerationEndTime = System.nanoTime();
+				System.out.println("Time to generate contigs(ms): " + (greedyContigGenerationEndTime - greedyContigGenerationStartTime) / 1000000);
 				gog = null;
 				System.gc();
 				break;
 				
-				/*
-				Thread t = new Thread() {
-					@Override
-					public void run() {
-						super.run();
-						System.out.println("Method: Greedy");
-						graphConstructionStartTime = System.nanoTime();
-						GreedyOverlapGraph gog = new GreedyOverlapGraph(MINIMUM_OVERLAP_LENGTH);
-						gog.constructGraph(new File(READS_FILE_NAME), MINIMUM_OVERLAP_LENGTH);
-						graphConstructionEndTime = System.nanoTime();
-						System.out.println("Time to construct graph(ms): " + (graphConstructionEndTime - graphConstructionStartTime) / 1000000);
-						
-						contigGenerationStartTime = System.nanoTime();
-						gog.traverseGraphToGenerateContigs(GENERATED_CONTIGS_FILE);
-						contigGenerationEndTime = System.nanoTime();
-						System.out.println("Time to generate contigs(ms): " + (contigGenerationEndTime - contigGenerationStartTime) / 1000000);
-						gog = null;
-						System.gc();
-					}
-					
-					public Thread initialize() {
-						return this;
-					}
-				}.initialize();
-				t.start();
-				long prev = 0, current;
-				while (t.isAlive()) {
-					current = java.lang.management.ManagementFactory.getThreadMXBean().getThreadCpuTime(t.getId());
-					if (current != -1)
-						prev = current;
-				}
-				System.out.println("Thread time(ms) = " + (prev/1000000));
-				break;
-				*/
 			case IMPROVED_DE_BRUIJN:
 				System.out.println("Method: ImprovedDeBruijnGraph");
-				graphConstructionStartTime = System.nanoTime();
+				impDbgGraphConstructionStartTime = System.nanoTime();
 				ImprovedDeBruijnGraph idbg = new ImprovedDeBruijnGraph();
 				idbg.constructGraph(new File(READS_FILE_NAME), KMER_SIZE);
-				graphConstructionEndTime = System.nanoTime();
-				System.out.println("Time to construct graph(ms): " + (graphConstructionEndTime - graphConstructionStartTime) / 1000000);
+				impDbgGraphConstructionEndTime = System.nanoTime();
+				System.out.println("Time to construct graph(ms): " + (impDbgGraphConstructionEndTime - impDbgGraphConstructionStartTime) / 1000000);
 				//DeBruijnGraph.getInstance().displayGraph();
-				contigGenerationStartTime = System.nanoTime();
+				impDbgContigGenerationStartTime = System.nanoTime();
 				idbg.traverseGraphToGenerateContigs(GENERATED_CONTIGS_FILE);
-				contigGenerationEndTime = System.nanoTime();
-				System.out.println("Time to generate contigs(ms): " + (contigGenerationEndTime - contigGenerationStartTime) / 1000000);
+				impDbgContigGenerationEndTime = System.nanoTime();
+				System.out.println("Time to generate contigs(ms): " + (impDbgContigGenerationEndTime - impDbgContigGenerationStartTime) / 1000000);
 				idbg = null;
 				System.gc();
 				break;
@@ -130,7 +106,70 @@ public class Main
 			}
 		}
 		
+		isCompleted = true;
 		programEndTime = System.nanoTime();
 		System.out.println("Program execution time(ms): " + (programEndTime - programStartTime) / 1000000);
+	}
+	
+	public static long getGraphConstructionRunTime(AssemblyMethods assemblyMethod) {
+		switch(assemblyMethod) {
+		case DE_BRUIJN:
+			if (dbgGraphConstructionStartTime != 0 && dbgGraphConstructionEndTime == 0)
+				return (System.nanoTime() - dbgGraphConstructionStartTime) / 1000000;
+			else
+				return (dbgGraphConstructionEndTime - dbgGraphConstructionStartTime) / 1000000;
+		
+		case OVERLAP:
+			if (overlapGraphConstructionStartTime != 0 && overlapGraphConstructionEndTime == 0)
+				return (System.nanoTime() - overlapGraphConstructionStartTime) / 1000000;
+			else
+				return (overlapGraphConstructionEndTime - overlapGraphConstructionStartTime) / 1000000;
+		
+		case GREEDY:
+			if (greedyGraphConstructionStartTime != 0 && greedyGraphConstructionEndTime == 0)
+				return (System.nanoTime() - greedyGraphConstructionStartTime) / 1000000;
+			else
+				return (greedyGraphConstructionEndTime - greedyGraphConstructionStartTime) / 1000000;
+			
+		case IMPROVED_DE_BRUIJN:
+			if (impDbgGraphConstructionStartTime != 0 && impDbgGraphConstructionEndTime == 0)
+				return (System.nanoTime() - impDbgGraphConstructionStartTime) / 1000000;
+			else
+				return (impDbgGraphConstructionEndTime - impDbgGraphConstructionStartTime) / 1000000;
+			
+		default:
+			return 0;
+		}
+	}
+	
+	public static long getContigGenerationRunTime(AssemblyMethods assemblyMethod) {
+		switch(assemblyMethod) {
+		case DE_BRUIJN:
+			if (dbgContigGenerationStartTime != 0 && dbgContigGenerationEndTime == 0)
+				return (System.nanoTime() - dbgContigGenerationStartTime) / 1000000;
+			else
+				return (dbgContigGenerationEndTime - dbgContigGenerationStartTime) / 1000000;
+		
+		case OVERLAP:
+			if (overlapContigGenerationStartTime != 0 && overlapContigGenerationEndTime == 0)
+				return (System.nanoTime() - overlapContigGenerationStartTime) / 1000000;
+			else
+				return (overlapContigGenerationEndTime - overlapContigGenerationStartTime) / 1000000;
+		
+		case GREEDY:
+			if (greedyContigGenerationStartTime != 0 && greedyContigGenerationEndTime == 0)
+				return (System.nanoTime() - greedyContigGenerationStartTime) / 1000000;
+			else
+				return (greedyContigGenerationEndTime - greedyContigGenerationStartTime) / 1000000;
+			
+		case IMPROVED_DE_BRUIJN:
+			if (impDbgContigGenerationStartTime != 0 && impDbgContigGenerationEndTime == 0)
+				return (System.nanoTime() - impDbgContigGenerationStartTime) / 1000000;
+			else
+				return (impDbgContigGenerationEndTime - impDbgContigGenerationStartTime) / 1000000;
+			
+		default:
+			return 0;
+		}
 	}
 }
--- src/ui/SummaryPanel.java
+++ src/ui/SummaryPanel.java
@@ -1,12 +1,17 @@
 package ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
@@ -15,19 +20,21 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.BevelBorder;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+
+import assembler.Main;
 
 public class SummaryPanel extends JPanel {
 	private static final long serialVersionUID = 3L;
 	private JTabbedPane tabbedPane;
 	final JFXPanel fxPanel;
-	private final static String greedy = "Greedy";
-	private final static String overlap = "Overlap";
-	private final static String deBruijn = "De Bruijn";
-	private final static String improvedDBG = "Improved De Bruijn";
+	private static String greedy = "Greedy";
+	private static String overlap = "Overlap";
+	private static String deBruijn = "De Bruijn";
+	private static String improvedDBG = "Improved De Bruijn";
 	private JButton btnPrevious;
 	private JButton btnExit;
+	static ObservableList<Series<String, Number>> observableList;
+	static BarChart<String, Number> barChart;
 	
 	public SummaryPanel(JTabbedPane tp) {
 		this.tabbedPane = tp;
@@ -42,6 +49,19 @@ public class SummaryPanel extends JPanel {
 			}
 		});
 		
+		btnPrevious = new JButton("Run!"); // TODO: Rename to Previous
+		btnPrevious.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//tabbedPane.setSelectedIndex(MainFrame.GenomeAssemblerTabIndex);
+				new Thread() {
+					public void run() {
+						startMonitoring();
+						Main.main(new String[0]);				
+					};
+				}.start();
+			}
+		});
+
 		btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -49,26 +69,20 @@ public class SummaryPanel extends JPanel {
 			}
 		});
 		
-		btnPrevious = new JButton("Previous");
-		btnPrevious.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				tabbedPane.setSelectedIndex(MainFrame.GenomeAssemblerTabIndex);
-			}
-		});
-		
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(536, Short.MAX_VALUE)
-					.addComponent(btnPrevious)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnPrevious)
+							.addPreferredGap(ComponentPlacement.RELATED, 532, Short.MAX_VALUE)
+							.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(22)
+							.addComponent(fxPanel, GroupLayout.PREFERRED_SIZE, 628, GroupLayout.PREFERRED_SIZE)))
 					.addContainerGap())
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(22)
-					.addComponent(fxPanel, GroupLayout.PREFERRED_SIZE, 628, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(30, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
@@ -88,43 +102,66 @@ public class SummaryPanel extends JPanel {
 		// This method is invoked on the JavaFX thread
 		Scene scene = createScene();
 		fxPanel.setScene(scene);
-
 	}
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	
+	private static void startMonitoring() {
+		new Thread() {
+			@Override
+			public void run() {
+				super.run();
+				while (!Main.isCompleted) {
+					try { Thread.sleep(100); } catch (InterruptedException e) { e.printStackTrace(); }
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							//barChart.setAnimated(false);
+							observableList.set(0, getGraphConstructionSeries());
+							observableList.set(1, getContigGenerationSeries());
+							//barChart.setAnimated(true);
+						}
+					});
+				}
+			}
+		}.start();
+	}
+	
+	@SuppressWarnings("unchecked")
 	private static Scene createScene() {
-		final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String,Number> bc = 
-            new BarChart<String,Number>(xAxis,yAxis);
-        bc.setTitle("Country Summary");
-        xAxis.setLabel("Country");       
-        yAxis.setLabel("Value");
- 
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Graph Construction");       
-        series1.getData().add(new XYChart.Data(greedy, 25601.34));
-        series1.getData().add(new XYChart.Data(overlap, 20148.82));
-        series1.getData().add(new XYChart.Data(deBruijn, 10000));
-        series1.getData().add(new XYChart.Data(improvedDBG, 35407.15));    
-        
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("Contig Generation");
-        series2.getData().add(new XYChart.Data(greedy, 57401.85));
-        series2.getData().add(new XYChart.Data(overlap, 41941.19));
-        series2.getData().add(new XYChart.Data(deBruijn, 45263.37));
-        series2.getData().add(new XYChart.Data(improvedDBG, 117320.16));
-        
-        XYChart.Series series3 = new XYChart.Series();
-        series3.setName("Thread Runtime");
-        series3.getData().add(new XYChart.Data(greedy, 45000.65));
-        series3.getData().add(new XYChart.Data(overlap, 44835.76));
-        series3.getData().add(new XYChart.Data(deBruijn, 18722.18));
-        series3.getData().add(new XYChart.Data(improvedDBG, 17557.31));
-        
-        Scene scene  = new Scene(bc,800,600);
-        bc.getData().addAll(series1, series2, series3);
-
+		CategoryAxis xAxis = new CategoryAxis();
+		xAxis.setLabel("Method");
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("Time (ms)");
+		barChart = new BarChart<String, Number>(xAxis, yAxis);
+		barChart.setTitle("Genome Assembly Methods");
+		barChart.setCategoryGap(20);
+		barChart.setBarGap(2);
+		barChart.setVerticalGridLinesVisible(false);
+		barChart.setAnimated(false);
+		
+		Scene scene = new Scene(barChart, 800, 600);
+		observableList = FXCollections.observableArrayList(getGraphConstructionSeries(), getContigGenerationSeries());
+		barChart.setData(observableList);
+		
 		return (scene);
+	}
+	
+	static Series<String, Number> getGraphConstructionSeries() {
+		BarChart.Series<String, Number> graphConstructionSeries = new BarChart.Series<String, Number>();
+		graphConstructionSeries.setName("Graph Construction");
+		graphConstructionSeries.getData().add(new BarChart.Data<String, Number>(deBruijn, Main.getGraphConstructionRunTime(Main.AssemblyMethods.DE_BRUIJN)));
+		graphConstructionSeries.getData().add(new BarChart.Data<String, Number>(overlap, Main.getGraphConstructionRunTime(Main.AssemblyMethods.OVERLAP)));
+		graphConstructionSeries.getData().add(new BarChart.Data<String, Number>(greedy, Main.getGraphConstructionRunTime(Main.AssemblyMethods.GREEDY)));
+		graphConstructionSeries.getData().add(new BarChart.Data<String, Number>(improvedDBG, Main.getGraphConstructionRunTime(Main.AssemblyMethods.IMPROVED_DE_BRUIJN)));
+		return graphConstructionSeries;
+	}
+	
+	static Series<String, Number> getContigGenerationSeries() {
+		BarChart.Series<String, Number> contigGenerationSeries = new BarChart.Series<String, Number>();
+		contigGenerationSeries.setName("Contig Generation");
+		contigGenerationSeries.getData().add(new BarChart.Data<String, Number>(deBruijn, Main.getContigGenerationRunTime(Main.AssemblyMethods.DE_BRUIJN)));
+		contigGenerationSeries.getData().add(new BarChart.Data<String, Number>(overlap, Main.getContigGenerationRunTime(Main.AssemblyMethods.OVERLAP)));
+		contigGenerationSeries.getData().add(new BarChart.Data<String, Number>(greedy, Main.getContigGenerationRunTime(Main.AssemblyMethods.GREEDY)));
+		contigGenerationSeries.getData().add(new BarChart.Data<String, Number>(improvedDBG, Main.getContigGenerationRunTime(Main.AssemblyMethods.IMPROVED_DE_BRUIJN)));
+		return contigGenerationSeries;
 	}
 }
